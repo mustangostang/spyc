69b2ae809f8507f93f5af76d5ff65c12b7480646
vlad.andersen <vlad.andersen@705174c8-340b-11de-9d05-31133e6853b1>
2009-04-30 13:27:26 +0000

    A testcase for <> symbols.
    git-svn-id: https://spyc.googlecode.com/svn/trunk@24 705174c8-340b-11de-9d05-31133e6853b1

--- spyc/tests/ParseTest.php
+++ spyc/tests/ParseTest.php
@@ -193,4 +193,8 @@ class ParseTest extends PHPUnit_Framework_TestCase {
       $this->assertEquals ($this->yaml['array on several lines'], array (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19));
     }
 
+    public function testmoreLessKey() {
+      $this->assertEquals ($this->yaml['morelesskey'], '<value>');
+    }
+
 }
